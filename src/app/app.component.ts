279b5c73d780bfa926d2a939ae60d1897050bb32
siddhesh953 <siddheshsawant953@gmail.com>
2020-09-09 14:06:56 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,40 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'myapp';
+
+   cardData = [
+    {
+      question: 'What is bootstrap in HTML?',
+      user: 'Deepak Yadav',
+      date: '05-Sep-2020',
+      answers: [
+        {
+          answer: 'I don\'t know.',
+          user: 'Mayank',
+          date: '05-Sep-2020',
+        },
+        {
+          answer: 'I don\'t know.',
+          user: 'Shubham',
+          date: '05-Sep-2020',
+        }
+      ],
+    },
+    {
+      question: 'What is HTML?',
+      user: 'Mayank Singh',
+      date: '05-Sep-2020',
+      answers: [
+        {
+          answer: 'I don\'t know buddy.',
+          user: 'Deepak',
+          date: '05-Sep-2020',
+        }
+      ],
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatDividerModule} from '@angular/material/divider';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AvatarModule } from 'ngx-avatar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatDividerModule,
+    MatButtonModule,
+    AvatarModule,
+    HttpClientModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
